9a1ba68854ea2ff04427407d66b80cf79830e154
Jude-Martin <62790847+Jude-Martin@users.noreply.github.com>
2026-04-06 00:05:11 +0100

    Create run_sim.sh

---
+++ run_sim.sh
@@ -0,0 +1,10 @@
+bash
+
+
+python run_evolution.py \
+  --config-tsv simulation_config.tsv \
+  --start-fasta start.fasta \
+  --orf-file orfs.tsv \
+  --reference-rscu reference.tsv \
+  --penalty-regions penalty_regions.tsv \
+  --output-dir output
