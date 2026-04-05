6344d6eafc2c9ea126b4dc0678aac08dbb7e7ede
Jude-Martin <62790847+Jude-Martin@users.noreply.github.com>
2026-04-05 22:30:43 +0100

    Create plot_sims.sh

---
+++ plot_sims.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+OUTDIR=combined_plots
+#activate venv
+
+python plot_evolution_metrics.py \
+  --summary-tsv run1/output/generation_summaries.tsv \
+  --label run1 \
+  --summary-tsv run2/output/generation_summaries.tsv \
+  --label run2 \
+  --summary-tsv run3/output/generation_summaries.tsv \
+  --label run3 \
+  --output-dir "$OUTDIR" \
+  --plots similarity dnds viable_parent_count novel_stop_count \
+  --ymax-similarity 0.5 \
+  --ymax-dnds 3.0 \
+  --ymax-viable-parent-count 100 \
+  --ymax-novel-stop-count 100000
